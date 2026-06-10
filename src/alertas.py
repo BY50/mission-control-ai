175a63ac1ad85dbb8aa51422c27cbb6824f05902
BY50 <yudimoritaka@gmail.com>
2026-06-09 23:14:04 -0300

    Created avaliar

--- src/alertas.py
+++ src/alertas.py
@@ -1,1 +1,16 @@
-
+def avaliar(dados):
+  counter = 0
+  aval = []
+  for d in dados:
+    if(counter == 0):
+      if(d > 50):
+        aval.append("Alerta - Temperatura Extrema, contatar corpo de bombeiros")
+      else:
+        aval.append("Temperatura normal")
+    elif(counter == 1):
+      if(d > 2000):
+        aval.append("Alerta - Desmatamento excessivo, contatar policia")
+      else:
+        aval.append("Desmatamento não muito grande")
+    counter = counter + 1
+  return aval    
