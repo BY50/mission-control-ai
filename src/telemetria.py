d8a3cf44bd42855da783b14b5de329d0d8b08b0e
BY50 <yudimoritaka@gmail.com>
2026-06-09 22:49:44 -0300

    Created coletar function

--- src/telemetria.py
+++ src/telemetria.py
@@ -1,1 +1,8 @@
-
+import random
+def coletar():
+  temperaturaMonitorada = random.randrange(-20, 300, 1)
+  hectaresDestruidos = random.randrange(0, 50000, 1)
+  energia = random.randrange(0, 100, 1)
+  dados = {"Temperatura monitorada": temperaturaMonitorada, "Hectares Desmatados": hectaresDestruidos, 
+           "Energia restante": energia}
+  return dados
